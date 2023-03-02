f4e093174fb65cb79d4dbb701924dd7b305f24c8
Paul Lorenz <paul.lorenz@netfoundry.io>
2023-03-02 15:09:30 -0500

    Fix build for js/wasm

--- identity.go
+++ identity.go
@@ -22,7 +22,6 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	"github.com/fsnotify/fsnotify"
 	"github.com/michaelquigley/pfxlog"
 	"github.com/openziti/foundation/v2/tlz"
 	"github.com/openziti/identity/certtools"
@@ -213,76 +212,6 @@ func (id *ID) getFiles() []string {
 	}
 
 	return files
-}
-
-// StopWatchingFiles decrements the number of watchers. If zero is hit all watching is stopped.
-// If too many stops are called a panic will occur.
-func (id *ID) StopWatchingFiles() {
-	if count := id.watchCount.Add(-1); count == 0 {
-		close(id.closeNotify)
-	} else if count < 0 {
-		logrus.Panicf("StopWatchingFiles called when not watching count is %d", count)
-	}
-}
-
-// WatchFiles will increment the number of watchers. The first watcher will start a
-// file system watcher. WatchFiles should match with a StopWatchingFiles.
-func (id *ID) WatchFiles() error {
-	if id.watchCount.Add(1) == 1 {
-		return id.startWatching()
-	}
-
-	return nil
-}
-
-// startWatching starts an internal file watcher
-func (id *ID) startWatching() error {
-	id.closeNotify = make(chan struct{})
-	files := id.getFiles()
-
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		return err
-	}
-
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					logrus.Error("identity file watcher received !ok from events, no further information")
-					return
-				}
-
-				logrus.Info("identity file watcher received event, queuing reload: " + event.String())
-				id.queueReload(id.closeNotify)
-
-			case err, ok := <-watcher.Errors:
-				if err != nil {
-					logrus.Errorf("identity file watcher received an error [%v]", err)
-				}
-
-				if !ok {
-					logrus.Error("identity file watcher received !ok from errors, no further information")
-					return
-				}
-			case <-id.closeNotify:
-				logrus.Info("identity file watcher closing")
-				return
-			}
-		}
-	}()
-
-	for _, file := range files {
-		err := watcher.Add(file)
-
-		if err != nil {
-			_ = watcher.Close()
-			close(id.closeNotify)
-		}
-	}
-
-	return nil
 }
 
 // Cert returns the ID's current client certificate that is used by all tls.Config's generated from it.
